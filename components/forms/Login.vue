7ffffc4e1de99d12c9956710e769e8a78c4548cd
raihanmd <mraihanf471@gmail.com>
2023-11-25 11:25:56 +0700

    idk bug in vercel

--- components/forms/Login.vue
+++ components/forms/Login.vue
@@ -8,18 +8,16 @@ const login = async () => {
 
 <template>
   <div class="w-full max-w-sm p-4 bg-white rounded-lg shadow-[0_30px_60px_rgba(0,0,0,0.15)] sm:p-6 md:p-8">
-    <form class="" @submit.prevent="login">
-      <div class="mb-6">
-        <h5 class="text-2xl font-medium text-gray-900 mb-2">Login</h5>
-        <span class="text-sm font-normal text-gray-500">Login to continue in our application</span>
-      </div>
-      <button
-        type="submit"
-        class="w-full flex justify-center items-center text-white bg-amber-800/70 hover:bg-amber-800/80 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mb-6 gap-4"
-      >
-        <i class="ri-google-fill text-lg"></i>
-        <span>Login with Google</span>
-      </button>
-    </form>
+    <div class="mb-6">
+      <h5 class="text-2xl font-medium text-gray-900 mb-2">Login</h5>
+      <span class="text-sm font-normal text-gray-500">Login to continue in our application</span>
+    </div>
+    <button
+      @click="login"
+      class="w-full flex justify-center items-center text-white bg-amber-800/70 hover:bg-amber-800/80 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mb-6 gap-4"
+    >
+      <i class="ri-google-fill text-lg"></i>
+      <span>Login with Google</span>
+    </button>
   </div>
 </template>
--- pages/login/index.vue
+++ pages/login/index.vue
@@ -11,7 +11,7 @@ definePageMeta({
 <template>
   <section>
     <div class="container">
-      <div class="flex justify-center items-center min-h-[calc(100vh-6rem)]">
+      <div class="flex justify-center items-center h-screen">
         <!-- //TODO Make anonym can see '/' -->
         <!-- <NuxtLink to="/" class="absolute top-4 left-4 bg-amber-800/70 text-white h-[45px] w-[45px] flex justify-center items-center rounded-full shadow-lg cursor-pointer">
           <i class="ri-arrow-left-line"></i>
