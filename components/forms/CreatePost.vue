d7b77ed13395e35250a4065b121c524035827d55
raihanmd <mraihanf471@gmail.com>
2023-11-27 21:55:03 +0700

    ascending false

--- components/forms/CreatePost.vue
+++ components/forms/CreatePost.vue
@@ -221,7 +221,7 @@ const deletePreview = () => {
       <div class="mb-6" v-show="hastags.length > 0">
         <div class="flex items-center gap-2 flex-wrap">
           <div v-for="(hastag, index) in hastags" :key="index" class="rounded-full p-1">
-            <div @click="removeHastag(hastag)" class="cursor-pointer badge badge-accent text-white gap-1">
+            <div @click="removeHastag(hastag)" class="cursor-pointer badge badge-accent text-white gap-1 font-quicksand">
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-4 h-4 stroke-current">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
               </svg>
--- server/api/post/index.ts
+++ server/api/post/index.ts
@@ -3,7 +3,7 @@ import { serverSupabaseClient } from "#supabase/server";
 export default defineEventHandler(async (event) => {
   const supabase = await serverSupabaseClient(event);
 
-  const { data } = await supabase.from("posts").select("id, title, url, hastags, createdAt, users ( name, image )").order("createdAt", { ascending: true });
+  const { data } = await supabase.from("posts").select("id, title, url, hastags, createdAt, users ( name, image )").order("createdAt", { ascending: false });
 
   return { data };
 });
