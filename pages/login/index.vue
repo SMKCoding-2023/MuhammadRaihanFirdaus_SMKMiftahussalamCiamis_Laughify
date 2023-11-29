6d706bec8ff3f64744781d49b1368e901868d673
raihanmd <mraihanf471@gmail.com>
2023-11-30 06:12:06 +0700

    link to / from /login

--- pages/login/index.vue
+++ pages/login/index.vue
@@ -12,10 +12,9 @@ definePageMeta({
   <section>
     <div class="container">
       <div class="flex justify-center items-center h-screen">
-        <!-- //TODO Make anonym can see '/' -->
-        <!-- <NuxtLink to="/" class="absolute top-4 left-4 bg-amber-800/70 text-white h-[45px] w-[45px] flex justify-center items-center rounded-full shadow-lg cursor-pointer">
+        <NuxtLink to="/" class="absolute top-4 left-4 bg-amber-800/70 text-white h-[45px] w-[45px] flex justify-center items-center rounded-full shadow-lg cursor-pointer">
           <i class="ri-arrow-left-line"></i>
-        </NuxtLink> -->
+        </NuxtLink>
         <FormsLogin />
       </div>
     </div>
